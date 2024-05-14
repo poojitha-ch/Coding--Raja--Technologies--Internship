91d7cfd535fb94c39d0f6613518dad18a098851a
poojitha-ch <poojithachalla.2004@gmail.com>
2024-05-14 08:53:04 +0530

    upd

---
+++ ecom1/vite-project/src/components/heroSection/HeroSection.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HeroSection() {
+  return (
+    <div>
+    <img src="https://chargebacks911.com/wp-content/uploads/2023/08/Online-vs-In-Store-Shopping-blog.jpg" alt="" />
+</div>
+  )
+}
+
+export default HeroSection
